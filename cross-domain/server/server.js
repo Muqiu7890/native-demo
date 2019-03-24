66794eb0fc626fe18d1c3422ed62f0cbbe14ad37
Muqiu7890 <1337651840@qq.com>
2019-03-24 23:59:06 +0800

    cross domain jsonp

---
+++ cross-domain/server/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/test',function (req,res) {
+    const {params,callback} = req.query
+    res.end(`${callback}('cross domain success')`)
+})
+
+app.listen(3000,function () {
+    console.log('app running at port 3000')
+})
