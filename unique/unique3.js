d770657fb534fddd4b0cd12bda8d9b65845891c0
Muqiu7890 <1337651840@qq.com>
2019-04-14 16:04:02 +0800

    modify unique3

--- unique/unique3.js
+++ unique/unique3.js
@@ -2,16 +2,9 @@
 // + 对于相等值得判断类似于三等运算符 主要区别NaN等于自身
 // + 在Set数据结构中 两个对象总是不相等的
 // + 将Set结构转为数组 Array.from()
-// function unique3(arr) {
-//     return [...new Set(arr)]
-// }
-//
-// console.log(unique3([1,2,3,1,2,4,56]))
+function unique3(arr) {
+    return [...new Set(arr)]
+}
 
-let set = new Set();
+console.log(unique3([1,2,3,1,2,4,56]))
 
-set.add({"a":1});
-console.log(set.size)
-
-set.add({"a":1});
-console.log(set.size)
