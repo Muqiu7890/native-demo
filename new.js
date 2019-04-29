79f2e2fcf9dd1eb84fc353716485061612ddad10
Muqiu7890 <1337651840@qq.com>
2019-04-29 20:52:27 +0800

    modify new

--- new.js
+++ new.js
@@ -2,11 +2,17 @@
 // - 将构造函数的作用域赋值给新对象(因此this指向这个对象)
 // - 执行构造函数中的代码(为这个新对象添加属性)
 // - 返回新对象
-function _new() {
-    var obj = {}
-    let Con = [].shift().call(arguments)
-    obj.__proto__ = Con.prototype
-    let res = Con.apply(obj,arguments)
-    return typeof res === 'object' ? res : obj
+function _new(func) {
+    let target = {}
+    target.__proto__ = func.prototype
+    let res = func.call(target)
+    if (typeof (res) == 'object' || typeof (res) == 'function') {
+        return res
+    }
+    return target
 }
+
+
+
+
 
